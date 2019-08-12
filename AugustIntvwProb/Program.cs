04b08d31a4630348b07a91aeec34ea4bc6df4a15
mikes <mikes@DESKTOP-EOUCCHM>
2019-08-12 19:58:33 -0400

    Add project files.

---
+++ AugustIntvwProb/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AugustIntvwProb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Check to see if a number is a palindrome.
+
+            //Console.WriteLine("Please enter a number: ");
+
+            //string userInt = Console.ReadLine();
+
+            //string userIntReversed = "";
+
+            //for (int i = userInt.Length - 1; i >= 0; i--)
+
+            //{
+
+            //    userIntReversed += userInt[i];
+
+            //}
+
+            //if (userIntReversed == userInt)
+
+            //{
+
+            //    Console.WriteLine("Your number is a palindrome!");
+
+            //}
+
+            //else 
+
+            //{
+
+            //    Console.WriteLine("Not a palindrome.");
+
+            //}
+        }
+    }
+}
