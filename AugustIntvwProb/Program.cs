c87905c7656cbd62f829419415aa54658deeda40
mikespriggs26 <mikespriggs@msn.com>
2019-08-13 18:49:50 -0400

    Additions

--- AugustIntvwProb/Program.cs
+++ AugustIntvwProb/Program.cs
@@ -11,36 +11,61 @@ namespace AugustIntvwProb
         static void Main(string[] args)
         {
             //Check to see if a number is a palindrome.
+            //Console.WriteLine("Please enter a number:");
+            //string num1 = Console.ReadLine();
+            //string num2 = "";
 
-            //Console.WriteLine("Please enter a number: ");
 
-            //string userInt = Console.ReadLine();
-
-            //string userIntReversed = "";
-
-            //for (int i = userInt.Length - 1; i >= 0; i--)
-
+            //for (int i =  num1.Length - 1; i >= 0; i--)
             //{
-
-            //    userIntReversed += userInt[i];
-
+            //    num2 += num1[i];
             //}
-
-            //if (userIntReversed == userInt)
-
+            //if(num1 == num2)
             //{
-
-            //    Console.WriteLine("Your number is a palindrome!");
-
+            //    Console.WriteLine("Your number is a palindrome.");
             //}
-
-            //else 
-
+            //else
             //{
-
-            //    Console.WriteLine("Not a palindrome.");
-
+            //    Console.WriteLine("Your number is not a palindrome.");
             //}
+            //Console.ReadKey();  
+            //*********************************************************
+
+
+            //Eliminate duplicate letters from a string
+            //Console.WriteLine("Please enter a word: ");
+            //string str = Console.ReadLine();
+            //string newStr = "";
+
+            //foreach(char j in str)
+            //{
+            //    if (newStr.Contains(j) == false)       //Could have used (!newStr.Contains(j))
+            //    {
+            //        newStr += j;
+            //    }
+            //}
+            //Console.WriteLine(newStr);
+            //Console.ReadKey();
+
+            //List all Armstrong Numbers
+            for (int i = 100; i < 101; i++)
+            {
+                int num1 = i % 10;
+                int num2 = i / 10;
+                int num3 = num2 % 10;
+                int num4 = i / 100;
+                Console.WriteLine(num1);
+                Console.WriteLine(num3);
+                Console.WriteLine(num4);
+
+                int sum = num1 * num1 * num1 * num3 * num3 * num3 * num4 * num4 * num4;
+                Console.WriteLine(sum);
+                if(sum == i)
+                {
+                    Console.WriteLine(i + " is an Armstrong Number!");
+                } 
+            }
+            Console.ReadKey();
         }
     }
 }
