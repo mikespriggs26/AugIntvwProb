0867011a768399eda6c929ee61b4ed287bb7ebc5
mikespriggs26 <mikespriggs@msn.com>
2019-08-14 18:45:45 -0400

    Added updates

--- AugustIntvwProb/Program.cs
+++ AugustIntvwProb/Program.cs
@@ -48,23 +48,73 @@ namespace AugustIntvwProb
             //Console.ReadKey();
 
             //List all Armstrong Numbers
-            for (int i = 100; i < 101; i++)
-            {
-                int num1 = i % 10;
-                int num2 = i / 10;
-                int num3 = num2 % 10;
-                int num4 = i / 100;
-                Console.WriteLine(num1);
-                Console.WriteLine(num3);
-                Console.WriteLine(num4);
+            //for (int i = 100; i < 101; i++)
+            //{
+            //    int num1 = i % 10;  //isolates the 3rd digit
+            //    int num2 = i / 10;  //isolates the 1st and 2nd digit for use in the next 2 calculations
+            //    int num3 = num2 % 10;   //isolates the 2nd number
+            //    int num4 = i / 100;     //isolates the 1st number
+            //    Console.WriteLine(num1);
+            //    Console.WriteLine(num3);
+            //    Console.WriteLine(num4);
 
-                int sum = num1 * num1 * num1 * num3 * num3 * num3 * num4 * num4 * num4;
-                Console.WriteLine(sum);
-                if(sum == i)
-                {
-                    Console.WriteLine(i + " is an Armstrong Number!");
-                } 
+            //    int sum = num1 * num1 * num1 * num3 * num3 * num3 * num4 * num4 * num4;
+            //    Console.WriteLine(sum);
+            //    if(sum == i)
+            //    {
+            //        Console.WriteLine(i + " is an Armstrong Number!");
+            //    } 
+            //}
+
+            //Determine if a number is prime
+            //Console.WriteLine("please enter a number: ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //for (int i = 2; i < num; i++)
+            //{
+
+            //    if (num % i == 0)
+            //    {
+            //        Console.WriteLine("your number is NOT prime.");
+            //        break;
+            //    }
+
+            //    else if (i == num - 1)
+            //    {
+            //        Console.WriteLine("Your number is prime.");
+            //    }
+            //}
+
+            //Find the largest number in an array
+            //int[] myArray = { 60, 10, 20, 30, 40, 50 };
+            //int highestNum = 0;
+
+            //foreach (int i in myArray)
+            //{
+            //    if(i > highestNum)
+            //    {
+            //        highestNum = i;
+            //    }
+            //}
+            //Console.WriteLine(highestNum);
+
+            //Print the nth number of a Fibonacci Series 
+            Console.WriteLine("Please enter a number");
+            int userNum = int.Parse(Console.ReadLine());
+            int firstNum = 0;
+            int secondNum = 1;
+            
+
+            for (int i = 2; i <= userNum; i++)
+            {
+                int result = firstNum + secondNum;
+                Console.WriteLine(result);
+                firstNum = secondNum;
+                secondNum = result;
+
             }
+
+
             Console.ReadKey();
         }
     }
